ea5fbfe710665a0549d24afd65b92182cff2851e
1286280961@qq.com <1286280961@qq.com>
2024-11-11 16:44:22 +0800

    登录和验证码接口开发

--- base-web/src/main/java/top/xym/web/sys_menu/service/impl/SysMenuServiceImpl.java
+++ base-web/src/main/java/top/xym/web/sys_menu/service/impl/SysMenuServiceImpl.java
@@ -15,12 +15,17 @@ import java.util.List;
 public class SysMenuServiceImpl extends ServiceImpl<SysMenuMapper, SysMenu> implements SysMenuService {
     @Override
     public List<SysMenu> getParent() {
+//        定义了一个字符串数组，表示两种可能的菜单类型。
         String[] type = {"0", "1"};
+//        将数组转换为一个列表，方便后续使用
         List<String> strings = Arrays.asList(type);
+//        创建查询包装器
         QueryWrapper<SysMenu> query = new QueryWrapper<>();
+//        设置查询条件
         query.lambda().in(SysMenu::getType, strings).orderByAsc(SysMenu::getOrderNum);
+//        执行查询
         List<SysMenu> menuList = this.baseMapper.selectList(query);
-        //组装顶级树
+        //组装顶级树  添加顶级菜单  创建一个新的SysMenu对象表示顶级菜单。设置了顶级菜单的各种属性，如标题、标签、父菜单 ID 和菜单 ID 等
         SysMenu menu = new SysMenu();
         menu.setTitle("顶级菜单");
         menu.setLabel("顶级菜单");
